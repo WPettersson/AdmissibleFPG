e3d404bbfc48a7b946d95584f8aba7ae2f565214
William Pettersson <william.pettersson@gmail.com>
2013-06-18 13:20:12 +1000

    Rename tests package

--- AdmissibleFPG/src/se/ewpettersson/admissiblefpg/tests/TestVertexConfig.java
+++ AdmissibleFPG/src/se/ewpettersson/admissiblefpg/tests/TestVertexConfig.java
@@ -1,4 +1,4 @@
-package tests;
+package se.ewpettersson.admissiblefpg.tests;
 
 import static org.junit.Assert.*;
 
--- AdmissibleFPG/src/se/ewpettersson/admissiblefpg/tests/testEdgeConfig.java
+++ AdmissibleFPG/src/se/ewpettersson/admissiblefpg/tests/testEdgeConfig.java
@@ -1,4 +1,4 @@
-package tests;
+package se.ewpettersson.admissiblefpg.tests;
 
 import java.util.ArrayList;
 import java.util.List;
