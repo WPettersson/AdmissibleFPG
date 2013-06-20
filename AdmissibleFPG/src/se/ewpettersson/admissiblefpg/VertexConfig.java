48e7f1db51b5500fc0e3b02c9f9cdc29b8007071
William Pettersson <william.pettersson@gmail.com>
2013-06-20 11:00:13 +1000

    Fix checking equality
    - Treat tet+vert as one Integer in equivalence classes
    - Make equivalence classes drop any tet+vert that are no longer on bdry
    - Add equals() function that only checks equiv. classes

--- AdmissibleFPG/src/se/ewpettersson/admissiblefpg/VertexConfig.java
+++ AdmissibleFPG/src/se/ewpettersson/admissiblefpg/VertexConfig.java
@@ -1,9 +1,12 @@
 package se.ewpettersson.admissiblefpg;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import se.ewpettersson.admissiblefpg.util.CircularListNode;
 
@@ -11,7 +14,7 @@ public class VertexConfig {
 	
 	// Maps a tet+vertex to a list of other pairs which have been squeezed together.
 	// Note that the entries in the list are formed using the "combine" function.
-	Map<Integer, Map<Integer, List<Integer>>> equiv; 
+	Map<Integer, List<Integer>> equiv; 
 	
 	// Maps a tet+vertex pair (formed using combine()) to a number indicating how many of the edges about
 	// this tet+vertex pair are on the boundary still.
@@ -20,23 +23,21 @@ public class VertexConfig {
 	
 	public void addTetrahedra(int id) {
 		
-		Map<Integer, List<Integer>> tet = new HashMap<Integer,List<Integer>>();
 		Map<Integer, Map<Integer, CircularListNode<TVE>>> tetLinks = new HashMap<Integer, Map<Integer, CircularListNode<TVE>>>();
 		
 		Map<Integer, CircularListNode<TVE>> vertLink;
 		List<Integer> vert;
 		for(int i=0; i<4; i++) {
-			vert = new LinkedList<Integer>();
+			vert = new ArrayList<Integer>();
 			vert.add(combine( id, i));
-			tet.put(i, vert);
+			equiv.put(combine(id, i),vert);
+
 			onBoundary.put(combine(id,i), 3);
 			
 			vertLink = new HashMap<Integer, CircularListNode<TVE>>();
 			tetLinks.put(i, vertLink);
 			
 		}
-		
-		equiv.put(id, tet);
 		links.put(id, tetLinks);
 		
 		createTriple(new TVE(id,0,-4), new TVE(id,0,5), new TVE(id,0,-6));
@@ -63,7 +64,7 @@ public class VertexConfig {
 	}
 
 	public VertexConfig() {
-		equiv = new HashMap<Integer,Map<Integer,List<Integer>>>();
+		equiv = new HashMap<Integer,List<Integer>>();
 		links = new HashMap<Integer,Map<Integer,Map<Integer,CircularListNode<TVE>>>>();
 		onBoundary = new HashMap<Integer, Integer>();
 	}
@@ -88,7 +89,7 @@ public class VertexConfig {
 		
 		if (! samePuncture) {
 			// The link-arcs must be on distinct punctures. Check if they're on the same vertex.
-			if ( equiv.get(gluing[0].tet).get(gluing[0].vertex).contains( combine( gluing[1].tet,gluing[1].vertex) ) ) {
+			if ( equiv.get(combine(gluing[0].tet,gluing[0].vertex)).contains( combine( gluing[1].tet,gluing[1].vertex) ) ) {
 				// Link arcs on distinct punctures, but same vertex.  Bad!
 				return false;
 			}
@@ -124,12 +125,12 @@ public class VertexConfig {
 		links.get(gluing[0].tet).get(gluing[0].vertex).remove(gluing[0].edge);
 		links.get(gluing[1].tet).get(gluing[1].vertex).remove(gluing[1].edge);
 		
-		List<Integer> equiv_list = equiv.get(gluing[0].tet).get(gluing[0].vertex);
+		List<Integer> equiv_list = equiv.get(combine(gluing[0].tet,gluing[0].vertex));
 		Integer combined = combine( gluing[1].tet,gluing[1].vertex);
 		if (!equiv_list.contains(combined)) {
 			equiv_list.add(combined);
 		}
-		equiv_list = equiv.get(gluing[1].tet).get(gluing[1].vertex);
+		equiv_list = equiv.get(combine(gluing[1].tet,gluing[1].vertex));
 		combined = combine( gluing[0].tet,gluing[0].vertex);
 		if (!equiv_list.contains(combined)) {
 			equiv_list.add(combined);
@@ -138,12 +139,30 @@ public class VertexConfig {
 		// Note that each of these tetrahedra/vertex pairs have one less boundary edge.
 		onBoundary.put(combine(gluing[0].tet,gluing[0].vertex),onBoundary.get(gluing[0].tet)-1);
 		onBoundary.put(combine(gluing[1].tet,gluing[1].vertex),onBoundary.get(gluing[1].tet)-1);
-
+		
+		// Remove them from all equivalence classes if they are no longer on the boundary	
+		combined = combine(gluing[0].tet,gluing[0].vertex);
+		if (onBoundary.get(combined) == 0) {
+			for( Integer i: equiv.get(combined)  ) {
+				List<Integer> otherList = equiv.get(i);
+				otherList.remove(i);
+			}
+			equiv.remove(combined);
+		}
+		
+		combined = combine(gluing[1].tet,gluing[1].vertex);
+		if (onBoundary.get(combined) == 0) {
+			for( Integer i: equiv.get(combined)  ) {
+				List<Integer> otherList = equiv.get(i);
+				otherList.remove(i);
+			}
+			equiv.remove(combined);
+		}
 			
 		return true;
 	}
 	
-	int combine(int tet, int vertex) {
+	Integer combine(int tet, int vertex) {
 		return 4*tet+vertex;
 	}
 	
@@ -202,9 +221,15 @@ public class VertexConfig {
 		return true;
 	}
 	
-	public boolean equalOnBoundary(VertexConfig other) {
-		
-		
+	public boolean equals(VertexConfig other) {
+		// Only need contents of equiv to match up.
+		for( Integer key: equiv.keySet()) {
+			Set<Integer> s = new HashSet<Integer>(equiv.get(key));
+			Set<Integer> s2 = new HashSet<Integer>(other.equiv.get(key));
+			if (! s.equals(s2)) {
+				return false;
+			}
+		}
 		return true;
 	}
 }
